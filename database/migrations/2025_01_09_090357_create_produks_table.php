4cf1c737f54100bc518fabc2557b5af1c5e9007f
Desta Sugara <mdestsgrr@gmail.com>
2025-01-09 16:42:38 +0700

    new

--- database/migrations/2025_01_09_090357_create_produks_table.php
+++ database/migrations/2025_01_09_090357_create_produks_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('produks', function (Blueprint $table) {
             $table->id();
+            $table->string('NamaProduk');
+            $table->decimal('Harga', 10, 2);
             $table->timestamps();
         });
     }
