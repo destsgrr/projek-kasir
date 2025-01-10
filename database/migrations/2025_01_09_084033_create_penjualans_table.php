16afe50c61876a234286535931702a04bcf8699d
Desta Sugara <mdestsgrr@gmail.com>
2025-01-10 11:14:21 +0700

    New

--- database/migrations/2025_01_09_084033_create_penjualans_table.php
+++ database/migrations/2025_01_09_084033_create_penjualans_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('penjualans', function (Blueprint $table) {
             $table->id();
+            $table->date('TanggalPenjualan');
+            $table->decimal('TotalHarga', 10, 2);
+            $table->unsignedBigInteger('UserId');
             $table->timestamps();
         });
     }
